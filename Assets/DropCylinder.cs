5780bada7e49efa49f7f1f5ddd9674a3d0a929db
Erifluffball <101990800+Erifluffball@users.noreply.github.com>
2025-10-09 10:03:52 +0100

    Update DropCylinder.cs

--- Assets/DropCylinder.cs
+++ Assets/DropCylinder.cs
@@ -2,15 +2,29 @@ using UnityEngine;
 
 public class DropCylinder : MonoBehaviour
 {
+    public GameObject obstacle;
+    GameObject[] agents;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        agents = GameObject.FindGameObjectsWithTag("agent");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hitInfo;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray.origin, ray.direction, out hitInfo))
+            {
+                Instantiate(obstacle, hitInfo.point, obstacle.transform.rotation);
+                //foreach (GameObject a in agents)
+                //{
+
+                //}
+            }
+        }
     }
 }
