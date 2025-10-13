f714c910ab21a9a905860950ea9a9a6a562223e8
Erifluffball <101990800+Erifluffball@users.noreply.github.com>
2025-10-13 13:01:28 +0100

    Flocking 1 - FlockManager.cs Created

---
+++ Assets/FlockManager.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class FlockManager : MonoBehaviour
+{
+    public GameObject fishPrefab;
+    public int numFish = 20;
+    public GameObject [] allFish;
+    public Vector3 swimLimits = new Vector3(5, 5, 5);
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        allFish = new GameObject [numFish];
+        for(int i=0; i < numFish; i++)
+        {
+            Vector 3 pos = this.transform.position + new Vector3(Random.Range(-swimLimits.x, swimLimits.x));
+                                                                (Random.Range(-swimLimits.y, swimLimits.y));
+                                                                (Random.Range(-swimLimits.z, swimLimits.z));
+            allFish[i] = Instantiate(fishPrefab, pos, Quaternion.identity);
+
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
